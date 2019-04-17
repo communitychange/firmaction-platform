65a0711fc4cab4e79a55b973f6e37e2fd485ca23
Nugraha <nugraha@indietechsolutions.com>
2019-04-18 06:30:08 +0700

    add new branch and initial commit

--- app/firm-vue-app/main.js
+++ app/firm-vue-app/main.js
@@ -9,7 +9,6 @@ Vue.component('nav-component', {
     "<nav id='nav' class='navbar navbar-fixed-top' data-spy='affix'><div><div class='navbar-header'><a href='#' class='navbar-brand'><img src='assets/images/logo.png' class='App-logo d-inline-block align-top' alt='logo' /></a><a class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'><i class='fa fa-bars'></i></a></div><div class='navbar-collapse collapse'><ul class='nav navbar-nav'><li><a href='#'>Link</a></li></ul><ul class='nav pull-right navbar-nav'><li><a href='#'>Link</a></li></ul></div></div></nav>"
 })
 
-
 Vue.component("button-component", {
   props: ["nbutton"]
 })
